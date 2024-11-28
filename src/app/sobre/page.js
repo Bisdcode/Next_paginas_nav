b0a8a5736b7784cfe94d0661d43b82f42b4389ac
Bisdcode <bismarck.sdcode@gmail.com>
2024-11-28 15:54:53 -0300

    initial commit

--- src/app/sobre/page.js
+++ src/app/sobre/page.js
@@ -1,13 +1,14 @@
-import Image from "next/image";
-import Link from "next/link";
+import Link from "next/link"
 
-export default function Home() {
+const SobrePage = () => {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-      Hello World!
+      Página de Sobre!
       </main> 
-      <Link href="/sobre">Sobre</Link>
+      <Link href="/">Ir para Home</Link>
     </div>
-  );
+  )
 }
+
+export default SobrePage

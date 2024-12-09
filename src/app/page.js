5d2ca07b73a41437b717b45f6dd299a01b38644c
Bisdcode <bismarck.sdcode@gmail.com>
2024-12-09 14:20:52 -0300

    alteracao da mensagem principal

--- src/app/page.js
+++ src/app/page.js
@@ -3,25 +3,25 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-      Hello World!
-      </main> 
-      <br />
-      <Link href="/sobre">Sobre</Link>
-      {/* Dados dinâmicos */}
-      <Link href="/posts">Posts</Link>
-      {/* Parâmetros na URL */}
-      <Link href="/exemplo?parametro=valor">Página com parâmetro</Link>
-      {/* Nested routes */}
-      <Link href="/produtos/categorias/roupas">Ir a categoria de Roupas</Link>
-      {/* Nested layouts */}
-      <Link href="/dashboard">Ir para Dashboard</Link>
-      {/* useRouter */}
-      <BotaoRedirect/>
-      {/* Redirect server */}
-      <Link href="/profile">Ir para meu perfil</Link>
-    </div>
-  );
+	return (
+		<div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+			<main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+				Hello World Nova Branch!
+			</main>
+			<br />
+			<Link href="/sobre">Sobre</Link>
+			{/* Dados dinâmicos */}
+			<Link href="/posts">Posts</Link>
+			{/* Parâmetros na URL */}
+			<Link href="/exemplo?parametro=valor">Página com parâmetro</Link>
+			{/* Nested routes */}
+			<Link href="/produtos/categorias/roupas">Ir a categoria de Roupas</Link>
+			{/* Nested layouts */}
+			<Link href="/dashboard">Ir para Dashboard</Link>
+			{/* useRouter */}
+			<BotaoRedirect />
+			{/* Redirect server */}
+			<Link href="/profile">Ir para meu perfil</Link>
+		</div>
+	);
 }
